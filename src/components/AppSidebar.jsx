8192d9d25cd47b1249913462c885c8cdb9ec609f
suneelkumarmaurya <suneelmaurya55@gmail.com>
2025-02-24 09:30:05 +0530

    only create side only not attach route in it

--- src/components/AppSidebar.jsx
+++ src/components/AppSidebar.jsx
@@ -10,20 +10,61 @@ import {
 } from "@/components/ui/sidebar";
 import { Link } from "react-router-dom";
 import logo from "../assets/images/logo-white.png";
+import { IoHome } from "react-icons/io5";
+import { BiSolidCategoryAlt } from "react-icons/bi";
+import { LiaBlogSolid } from "react-icons/lia";
+import { FaComments } from "react-icons/fa";
+import { FaUserFriends } from "react-icons/fa";
+import { GoDot } from "react-icons/go";
+
 const AppSidebar = () => {
   return (
     <Sidebar>
       <SidebarHeader className="bg-white">
-        <img src={logo} alt="logo" width={120}/>
+        <img src={logo} alt="logo" width={120} />
       </SidebarHeader>
       <SidebarContent className="bg-white">
         <SidebarGroup>
           <SidebarMenu>
             <SidebarMenuItem>
-              <SidebarMenuButton asChild>
-                <a href="">
-                  <span>Home</span>
-                </a>
+              <SidebarMenuButton>
+                <IoHome />
+                <Link to="/">Home</Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <SidebarMenuButton>
+                <BiSolidCategoryAlt />
+                <Link to="/">Category</Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <SidebarMenuButton>
+                <LiaBlogSolid />
+                <Link to="/">Blogs</Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <SidebarMenuButton>
+                <FaComments />
+                <Link to="/">Comments</Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <SidebarMenuButton>
+                <FaUserFriends />
+                <Link to="/">Users</Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroup>
+        <SidebarGroup>
+          <sidebarGroupLabel>categories</sidebarGroupLabel>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton>
+                <GoDot />
+                <Link to="/">Home</Link>
               </SidebarMenuButton>
             </SidebarMenuItem>
           </SidebarMenu>
