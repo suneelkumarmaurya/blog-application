152e0e76699980ee5d8684b1f07011698da0a8cd
suneelkumarmaurya <suneelmaurya55@gmail.com>
2025-02-24 11:08:12 +0530

    add topbar,footer and google font

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import { SidebarProvider } from '../components/ui/sidebar'
-import AppSidebar from '../components/AppSidebar'
-import { Outlet } from 'react-router-dom'
-import Footer from '../components/Footer'
-import Topbar from '../components/Topbar'
+import React from "react";
+import { SidebarProvider } from "../components/ui/sidebar";
+import AppSidebar from "../components/AppSidebar";
+import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
+import Topbar from "../components/Topbar";
 
 const Layout = () => {
   return (
     <SidebarProvider>
-        <Topbar/>
-        <AppSidebar/>
-        <main>
-            <Outlet/>
-            <Footer/>
-        </main>
+      <Topbar />
+      <AppSidebar />
+      <main className="w-full">
+        <div className="min-h-[calc(100vh-58px)]  py-4 px-5 ">
+          <Outlet />
+        </div>
+        <Footer />
+      </main>
     </SidebarProvider>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-      Footer
+    <div className="bg-gray-50 py-4 text-center text-sm">
+    © Copyright 2024 | Designed & Developed By:Suneel Maurya
     </div>
   )
 }
